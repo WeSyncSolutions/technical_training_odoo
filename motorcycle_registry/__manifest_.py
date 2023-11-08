a99f7ad14f8f5b15fa4bc25650b44683186005bc
william1089 <william.suarez@comaderas.com>
2023-11-08 03:22:30 +0000

    Creaación de nuevo modulo de Kaiil Motorcycle

---
+++ motorcycle_registry/__manifest_.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Motorcycle Registry',
+    'category': 'Kawiil/Custom Module',
+    'author': "william1089",
+    'summary': 'This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand',
+    'description': """Motorcycle Registry""",
+    'version': '0.0.1.',
+    'license': 'OPL-1',
+    'depends': ['payment'],
+    'images': ['static/description/icon.png'],
+    'application': True,
+    'installable': True,
+    'website': 'https://github.com/william1089/technical_training_odoo.git',
+}
