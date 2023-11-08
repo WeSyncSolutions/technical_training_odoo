fb8a8644459247c103cb042a1cc674bec1eecdc3
William <willy.suarezp@gmail.com>
2023-11-07 23:52:45 -0500

    Creación de archivo __init__.py

--- motorcycle_registry/__manifest_.py
+++ motorcycle_registry/__manifest_.py
@@ -1,11 +1,10 @@
 {
     'name': 'Motorcycle Registry',
-    'category': 'Kawiil/Custom Module',
+    'category': 'Kawiil/Custom Module Motorcycle',
     'author': "william1089",
     'summary': 'This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand',
     'version': '0.0.1.',
-    'license': 'OPL-1',
-    'depends': ['payment'],
+    'license': 'OPL-1', 
     'images': ['static/description/icon.png'],
     'application': True,
     'installable': True,
