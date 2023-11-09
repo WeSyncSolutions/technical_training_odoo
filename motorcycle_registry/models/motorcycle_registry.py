8d39e30624fecf5af0e231b8f1c1ca4e1bd000d1
William <willy.suarezp@gmail.com>
2023-11-08 20:16:46 -0500

    [ADD]fix_errors_model

--- motorcycle_registry/models/motorcycle_registry.py
+++ motorcycle_registry/models/motorcycle_registry.py
@@ -9,15 +9,12 @@ class MotorcyleRegistry(models.Model):
         ('vin_unique', 'UNIQUE(vin)','Odoopise! Another registration for this VIN number already exists.')
     ]
     
+    certificate_title = fields.Binary(string='Certificate of Title')
+    current_mileage = fields.Float(string='Current Mileage')
+    first_name = fields.Char(string='First Name', required=True)
+    last_name = fields.Char(string='Last Name', required=True)
+    license_plate = fields.Char(string='License Plate Number')
+    registry_date = fields.Date('Registration Date')
     registry_number = fields.Char('Registry Number', copy=False, required=True, readonly=True, default='New')
     vin = fields.Char(string='VIN', required=True)
-    first_name = fiels.Char(string='First Name', required=True)
-    last_name = fiels.Char(string='Last Name', required=True)
     picture = fields.Image (string='Photograph')
-    current_mileage = fields.Float(string='Current Mileage')
-    license_plate = fields.Char(string='License Plate Number')
-    certificate_title = fields.Binary(string='Certificate of Title')
-    registry_date = fields.Date('Registration Date')
-    
-    #Owner Fields
-    owner_id = fields.Many2One(comodel_name = 'res.partner', ondelete='restrict')
