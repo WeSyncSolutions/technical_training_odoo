6da68c09d0589c665d0dd53eb3c558b60e2aa886
William <willy.suarezp@gmail.com>
2023-11-14 12:29:47 -0500

    [ADD]pattern_for_fields

--- motorcycle_registry/models/motorcycle_registry.py
+++ motorcycle_registry/models/motorcycle_registry.py
@@ -1,10 +1,11 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, re
 from odoo.exceptions import ValidationError
 
 class MotorcycleRegistry(models.Model):
     _name = 'motorcycle.registry'
     _description = 'Motorcycle Registry'
-    _rec_name = "registry_number"   
+    _rec_name = "registry_number"
+    _sql_constraints = [('vin_unique', 'UNIQUE(vin)', 'VIN must be unique, please verify the list')]
     
     name = fields.Char(string="Motorcycle Registry")
     
@@ -19,9 +20,18 @@ class MotorcycleRegistry(models.Model):
     picture = fields.Image (string='Photograph')
     active = fields.Boolean(default=True)
     
+       
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
             if vals.get('registry_number', ('MRN0000')) == ('MRN0000'):
                 vals['registry_number'] = self.env['ir.sequence'].next_by_code('registry.number')
+            
+            pattern_license_plate = '^[A-Z]{1,3}\d{1,4}[A-Z]{0,2}$'
+            if 'license_plate' in vals and not re.match(pattern_license_plate, vals['license_plate']):
+                raise ValidationError('Número de Placa Inválido')
+
+            pattern_VIN = '^[A-Z]{4}\d{2}[A-Z0-9]{2}\d{5}$'
+            if 'vin' in vals and not re.match(pattern_VIN, vals['vin']):
+                raise ValidationError('Número de VIN Inválido')
         return super().create(vals_list)
