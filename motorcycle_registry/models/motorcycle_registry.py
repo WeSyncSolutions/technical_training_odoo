e9ce82321a16379dd10bdadd4c5bc8ceabdbc36b
William <willy.suarezp@gmail.com>
2023-11-13 21:06:56 -0500

    [ADD]sequence

--- motorcycle_registry/models/motorcycle_registry.py
+++ motorcycle_registry/models/motorcycle_registry.py
@@ -7,16 +7,7 @@ class MotorcycleRegistry(models.Model):
     _rec_name='registry_number'
     
     name = fields.Char(string="Registry")
-    
     registry_number = fields.Char(string='Registry Number', default="MRN0000", copy=False, required=True, readonly=True)
-    
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            if vals.get('registry_number',('MRN0000')) == ('MRN0000'):
-                vals['registry_number'] = self.env['ir.sequence'].next_by_code('registry_sequence')
-        return super().create(vals_list)       
-    
     certificate_title = fields.Binary(string='Certificate of Title')
     current_mileage = fields.Float(string='Current Mileage')
     first_name = fields.Char(string='First Name', required=True)
@@ -26,3 +17,10 @@ class MotorcycleRegistry(models.Model):
     vin = fields.Char(string='VIN', required=True)
     picture = fields.Image (string='Photograph')
     active = fields.Boolean(default=True)
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('registry_number',('MRN0000')) == ('MRN0000'):
+                vals['registry_number'] = self.env['ir.sequence'].next_by_code('registry_sequence')
+        return super().create(vals_list)  
