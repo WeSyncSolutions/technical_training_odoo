031cf0ca4026e4d4401314920f91d4a6fac89921
William <willy.suarezp@gmail.com>
2023-11-13 21:21:30 -0500

    [ADD]sequence

--- motorcycle_registry/models/motorcycle_registry.py
+++ motorcycle_registry/models/motorcycle_registry.py
@@ -5,7 +5,7 @@ class MotorcycleRegistry(models.Model):
     _name = 'motorcycle.registry'
     _description = 'Motorcycle Registry'
     
-    name = fields.Char(string="Registry")
+    #name = fields.Char(string="Registry")
     registry_number = fields.Char(string='Registry Number', default="MRN0000", copy=False, required=True, readonly=True)
     certificate_title = fields.Binary(string='Certificate of Title')
     current_mileage = fields.Float(string='Current Mileage')
@@ -21,5 +21,5 @@ class MotorcycleRegistry(models.Model):
     def create(self, vals_list):
         for vals in vals_list:
             if vals.get('registry_number',('MRN0000')) == ('MRN0000'):
-                vals['registry_number'] = self.env['ir.sequence'].next_by_code('registry.number')
+                vals['registry_number'] = self.env['ir.sequence'].next_by_code('registry.number') or 'MRN0000'
         return super().create(vals_list)  
